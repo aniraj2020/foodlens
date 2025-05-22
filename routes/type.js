fefa8c1f96fac71ea7ad9f79e7ca8bf1c5486a8a
aniraj2020 <aniraj2020@gmail.com>
2025-05-23 04:33:19 +1000

    Made UI adjustments in all the pages for consistency and appearance; major updates in public/style.css

--- routes/type.js
+++ routes/type.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getInsecurityTypeDistribution } = require("../controllers/foodSecurityController");
 
-// ✅ Route: GET /api/type → fetch food insecurity type distribution
+// Route: GET /api/type → fetch food insecurity type distribution
 router.get("/", getInsecurityTypeDistribution);
 
 module.exports = router;
